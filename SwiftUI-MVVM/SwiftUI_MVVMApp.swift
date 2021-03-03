6af06ebbf87c4d3b0869c98e8e1e5700fe05e36e
Nick Lin <hawhaw.ya@gmail.com>
2021-03-03 11:02:39 +0800

    Initial Commit

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Nick on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
